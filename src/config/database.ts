772c47c95b8e629b9e096c5fbfc155155be2e2af
Faril Mammadov <memmedovfaril@gmail.com>
2026-08-12 21:09:29 +0400

    feat: database config create

---
+++ src/config/database.ts
@@ -0,0 +1,19 @@
+import { Pool } from "pg";
+import { env } from "./env";
+
+export const pool = new Pool( {
+  host: env.db.host,
+  port: env.db.port,
+  database: env.db.name,
+  user: env.db.user,
+  password: env.db.password
+});
+
+pool.on('connect', () => {
+  console.log('✅ Connected to PostgreSQL');
+});
+
+pool.on('error', (err) => {
+  console.error('❌ PostgreSQL error:', err);
+  process.exit(1);
+});
