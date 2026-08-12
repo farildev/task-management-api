bb1cb54af05aa4c7602c51a9c911a7ee7c9aa6f7
Faril Mammadov <memmedovfaril@gmail.com>
2026-08-13 01:44:24 +0400

    feat: middleware implement to logout controller

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
-import { Router } from "express";
-import { AuthController } from "../controllers/auth.controller";
+import { Router } from 'express';
+import { AuthController } from '../controllers/auth.controller';
+import { authMiddleware } from '../middleware/auth.middleware';
 
 const router = Router();
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
-router.post('/logout', AuthController.logout);
+router.post('/logout', authMiddleware, AuthController.logout);
 
 export default router;
