472c21cce063f69feb85c2deef23d28243631ca3
Faril Mammadov <memmedovfaril@gmail.com>
2026-08-13 00:48:10 +0400

    feat: middleware and route add

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
-
+import router from "./routes";
+import { errorMiddleware } from "./middleware/error.middleware";
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(errorMiddleware);
+
+app.use('/api', router);
 
 app.get('/health', (req, res) => {
   res.json({status: 'ok'})
