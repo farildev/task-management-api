85dda53d2231335cd04a22180c73967898977e41
Faril Mammadov <memmedovfaril@gmail.com>
2026-08-17 01:21:37 +0400

    fea: all routes add to index route file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import authRouter from "./auth.routes";
-
+import projectsRouter from "./project.routes";
+import tasksRouter from "./task.routes"
+import commentsRouter from "./comments.routes";
 const router = Router();
 
 router.use('/auth', authRouter);
+router.use('/projects', projectsRouter);
+router.use('/projects/:projectId/tasks', tasksRouter);
+router.use('/tasks/:taskId/comments', commentsRouter);
 
 export default router;
